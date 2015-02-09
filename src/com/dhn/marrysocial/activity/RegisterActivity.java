d6f05a1985485d6788403171c28f7d2dc27c1bea
nannan <swinter.zhang@gmail.com>
2015-02-09 19:08:41 +0800

    fix the bug: optimize the UI, 1:actionbar and head pic

--- src/com/dhn/marrysocial/activity/RegisterActivity.java
+++ src/com/dhn/marrysocial/activity/RegisterActivity.java
@@ -124,32 +124,33 @@ public class RegisterActivity extends Activity implements OnClickListener {
     public void onClick(View arg0) {
         switch (arg0.getId()) {
         case R.id.register_btn: {
-
-            if (!Utils.isActiveNetWorkAvailable(this)) {
-                mHandler.sendEmptyMessage(NETWORK_INVALID);
-                return;
-            }
-
-            if (!isPhoneNumValid()) {
-                mPhoneNumEditText.requestFocus();
-                return;
-            }
-            if (!isPasswordValid()) {
-                mPasswordEditText.requestFocus();
-                return;
-            }
+            startToFillUserinfo();
+            return;
+//            if (!Utils.isActiveNetWorkAvailable(this)) {
+//                mHandler.sendEmptyMessage(NETWORK_INVALID);
+//                return;
+//            }
+//
+//            if (!isPhoneNumValid()) {
+//                mPhoneNumEditText.requestFocus();
+//                return;
+//            }
+//            if (!isPasswordValid()) {
+//                mPasswordEditText.requestFocus();
+//                return;
+//            }
 //            if (!isAuthCodeValid()) {
 //                return;
 //            }
-            mUploadProgressDialog = ProgressDialog.show(this, "用户注册",
-                    "正在注册，请稍后...", false, true);
-            mPassword = MD5SecretUtils.encrypt(mPasswordEditText.getText()
-                    .toString());
-            String macAddr = Utils.getMacAddress(this);
-            mPhoneNum = mPhoneNumEditText.getText().toString();
-            mExecutorService.execute(new RegisterUserInfo(mPhoneNum, mPassword,
-                    macAddr));
-            break;
+//            mUploadProgressDialog = ProgressDialog.show(this, "用户注册",
+//                    "正在注册，请稍后...", false, true);
+//            mPassword = MD5SecretUtils.encrypt(mPasswordEditText.getText()
+//                    .toString());
+//            String macAddr = Utils.getMacAddress(this);
+//            mPhoneNum = mPhoneNumEditText.getText().toString();
+//            mExecutorService.execute(new RegisterUserInfo(mPhoneNum, mPassword,
+//                    macAddr));
+//            break;
         }
         case R.id.login_btn: {
             startToLogin();
