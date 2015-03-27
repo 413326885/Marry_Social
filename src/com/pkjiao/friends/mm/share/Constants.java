e1e44f35d1eda45295fa075a9ace7a2225abfab2
nannan <swinter.zhang@gmail.com>
2015-03-27 10:41:46 +0800

    fix the bug: change the appid of wechat

--- src/com/pkjiao/friends/mm/share/Constants.java
+++ src/com/pkjiao/friends/mm/share/Constants.java
@@ -1,7 +1,7 @@
 package com.pkjiao.friends.mm.share;
 
 public class Constants {
-    public static String WX_APP_ID = "wx0bf9370f239a8969";
+    public static String WX_APP_ID = "wx53e204b51bbaf77f";
     public static String QQ_APP_ID = "1104433372";
     public static String QQ_APP_KEY = "d6vjKPxakcAF5Kgi";
     
